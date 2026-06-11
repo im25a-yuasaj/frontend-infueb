6364974fea2ec1ee515811eeb4271e769baca771
Josh Yuasa <yuasaj@bzz.ch>
2026-06-11 11:52:38 +0200

    resolve a probleme

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -5,19 +5,19 @@ import UserForm from '../../components/UserForm';
 
 
 async function UserPage() {
-    const data = await getDataFromServer('user', '');
+    const data = await getDataFromServer('user', '') as unknown as User[];
     console.log('Fetched data:', data);
+
     return (
         <div className="flex flex-col items-center justify-center h-full">
             <Link href="/" className="underline">return to Home</Link>
             <form>
-                <label>Alle Benutzer: </label>
                 <select name="user" id="user">
                     {data.map((user: User) => (
-                            <option key={user.BenutzerID}>
-                                {user.BenutzerName}
-                            </option>
-                        ))}
+                        <option key={user.BenutzerID}>
+                            {user.BenutzerName}
+                        </option>
+                    ))}
                 </select>
             </form>
             <UserForm />
